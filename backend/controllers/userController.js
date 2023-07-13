e47df1fb13426bfc7858030fd00d401032cf9f67
Joshua <jlmeeks587@gmail.com>
2023-07-13 17:29:11 -0400

    Finish project

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,12 +1,26 @@
 import asyncHandler from 'express-async-handler';
-import generateToken from '../utils/generateToken.js';
 import User from '../models/userModel.js';
+import generateToken from '../utils/generateToken.js';
 
 // @desc    Auth user/set token
 // route    POST /api/users/auth
 // @access  Public
 const authUser = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: 'Auth user' });
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (user && (await user.matchPassword(password))) {
+        generateToken(res, user._id)
+        return res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+        });
+    } else {
+        res.status(401);
+        throw new Error('Invalid email or password');
+    }
 });
 
 // @desc    Register a new user
@@ -16,7 +30,7 @@ const registerUser = asyncHandler(async (req, res) => {
     const { name, email, password } = req.body;
 
     const userExists = await User.findOne({ email });
-
+    
     if (userExists) {
         res.status(400);
         throw new Error('User already exists')
@@ -45,21 +59,51 @@ const registerUser = asyncHandler(async (req, res) => {
 // route    POST /api/users/logout
 // @access  Public
 const logoutUser = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: 'Logout user' });
+    res.cookie('jwt', '', {
+        httpOnly: true,
+        expires: new Date(0)
+    })
+    res.status(200).json({ message: 'User logged out' });
 });
 
 // @desc    Get user profile
 // route    GET /api/users/profile
 // @access  Private
 const getUserProfile = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: 'User profile' });
+    const user = {
+        _id: req.user._id,
+        name: req.user.name,
+        email: req.user.email
+    }
+
+    res.status(200).json(user);
 });
 
 // @desc    Update user profile
 // route    PUT /api/users/auth
 // @access  Public
 const updateUserProfile = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: 'Update user profile' });
+    const user = await User.findById(req.user._id);
+
+    if (user) {
+        user.name = req.body.name || user.name;
+        user.email = req.body.email || user.email;
+
+        if (req.body.password) {
+            user.password = req.body.password;
+        }
+
+        const updatedUser = await user.save();
+
+        res.status(200).json({
+            _id: updatedUser._id,
+            name: updatedUser.name,
+            email: updatedUser.email
+        })
+    } else {
+        res.status(404);
+        throw new Error('User not found');
+    }
 });
 
 export { 
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,32 +1,39 @@
 import mongoose from 'mongoose';
 import bcrypt from 'bcryptjs';
 
-const userSchema = mongoose.Schema({
-    name: {
-        type: String,
-        required: true
+const userSchema = mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        password: {
+            type: String,
+            required: true
+        },
     },
-    email: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    name: {
-        type: String,
-        required: true
-    },
-}, {
-    timestamps: true
-});
+    {
+        timestamps: true
+    }
+);
 
-userSchema.pre('save', async function(next) {
+userSchema.methods.matchPassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+};
+
+userSchema.pre('save', async function (next) {
     if (!this.isModified('password')) {
         next();
     }
 
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
-})
+});
 
 const User = mongoose.model('User', userSchema);
 
